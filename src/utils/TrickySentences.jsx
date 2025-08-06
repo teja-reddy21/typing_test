c07869e565e4d0d9b78536ffb043fffbdbc946cb
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-08-06 22:14:11 +0530

    data

---
+++ src/utils/TrickySentences.jsx
@@ -0,0 +1,22 @@
+// src/utils/trickySentences.js
+export const trickySentences = {
+  a: [
+    "An agile cat ran across the amazing avenue.",
+    "Always aim at achieving amazing ambitions."
+  ],
+  b: [
+    "Bob bought blue balloons before breakfast.",
+    "Brilliant boys build big bridges bravely."
+  ],
+  c: [
+    "Coding can create countless creative concepts.",
+    "Careful cats climb cautiously."
+  ],
+  // Add for all keys
+
+  
+};
+
+
+ 
+
