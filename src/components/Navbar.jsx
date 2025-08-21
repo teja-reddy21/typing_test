f42483c6f47ae41d7c4493ddef1d7f31d72a1825
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-08-21 21:43:05 +0530

    added small navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+
 const Navbar = ({ darkMode, setDarkMode}) => {
-  
+    const [open, setOpen] = useState(false);
   return (
-   <div className="sidebar">
-      <h2>⌨ Typing App</h2>
-      <nav>
-        <Link to="/">Typing Test</Link>
-        <Link to="/practice">Typing Practice</Link>
-        <Link to="/tricky">Tricky Keys</Link>
-      </nav>
-      <button onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
+    <>
+        <button className="menu-btn" onClick={() => setOpen(!open)}>
+        ☰
       </button>
-    </div>
+
+      <nav className={`sidebar ${open ? "open" : ""}`}>
+        <h2 className="logo">Typing App</h2>
+        <ul>
+          <li><Link to="/" onClick={() => setOpen(false)}>Typing Test</Link></li>
+          <li><Link to="/practice" onClick={() => setOpen(false)}>Typing Practice</Link></li>
+          <li><Link to="/tricky" onClick={() => setOpen(false)}>Tricky Keys</Link></li>
+        </ul>
+
+        {/* Dark Mode Toggle */}
+        <div className="dark-toggle">
+          <label>
+            <input
+              type="checkbox"
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+            Dark Mode
+          </label>
+        </div>
+      </nav>
+    </>
   )
 }
 
